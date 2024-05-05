a44b337d8d801351f10eb6b096287409137617c5
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-05-05 15:01:23 +0530

    Adde first job object and queue

---
+++ src/jobs/SampleJob.ts
@@ -0,0 +1,25 @@
+import { Job } from "bullmq";
+
+import { IJob } from "../types/bullMQJobDefinition";
+
+export default class SampleJob implements IJob {
+  name: string;
+
+  payload?: Record<string, unknown> | undefined;
+
+  constructor(payload: Record<string, unknown>) {
+    this.payload = payload;
+    this.name = this.constructor.name;
+  }
+
+  handle = () => {
+    console.log("Handler of the job called");
+  };
+
+  failed = (job?: Job): void => {
+    console.log("Job Failed");
+    if (job) {
+      console.log(job.id);
+    }
+  };
+}
